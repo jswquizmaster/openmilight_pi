d19776e2891a724b202ac67430ab8b17850bb071
jswquizmaster <dont@know.net>
2018-09-16 12:39:12 +0200

    Added scripts to train and remove devices

---
+++ remove.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./openmilight -d -q $1 -r $1 -c BA -b b8 -k 01 -v CC -n 20; ./openmilight -d -q $1 -r $1 -c BA -b b8 -k 11 -v CD -n 20; ./openmilight -d -q $1 -r $1 -c BA -b b8 -k 11 -v CE -n 20
---
+++ training.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./openmilight -d -q $1 -r $1 -c BA -b b8 -k 03 -v CC -n 20; ./openmilight -d -q $1 -r $1 -c BA -b b8 -k 13 -v CD -n 20; ./openmilight -d -q $1 -r $1 -c BA -b b8 -k 13 -v CE -n 20
